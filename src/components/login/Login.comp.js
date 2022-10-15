85761e933f32869ad582588d3596fb7f9cc317b2
Mateusz Izdebski <mati2k51@gmail.com>
2022-10-16 00:11:32 +0200

    Add set up. added loging and reset password form

---
+++ src/components/login/Login.comp.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+import { PropTypes } from "prop-types";
+
+
+export const LoginForm = ({handleOnchange,handleOnSubmit,formSwitcher, email, pass}) => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>   
+                        <h1 className="text-info text-center">Client Login</h1>
+                        <hr />
+                        <Form autoComplete="off" onSubmit={handleOnSubmit}>
+                            <Form.Group>
+                                <Form.Label>
+                                    Email Addres
+                                </Form.Label>
+                                <Form.Control type="email"
+                                name="email" 
+                                value={email}
+                                onChange={handleOnchange}
+                                placeholder="Enter Email" 
+                                required
+                                />
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>
+                                    Password
+                                </Form.Label>
+                                <Form.Control type="password"
+                                 name="password"
+                                 value={pass}
+                                 onChange={handleOnchange}
+                                 placeholder="Enter password"
+                                 required 
+                                 />
+                            </Form.Group>
+
+                            <Button type="submit">Login</Button>
+
+
+                        </Form>
+                        <hr />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <a href="hgy" onClick={() => formSwitcher('reset')}>Forget Password?</a>
+                    </Col>
+                </Row>
+                
+            </Container>
+        </div>
+    )
+}
+
+LoginForm.propTypes = {
+    handleOnchange: PropTypes.func.isRequired,
+    handleOnSubmit: PropTypes.func.isRequired,
+    formSwitcher: PropTypes.func.isRequired,
+    email: PropTypes.string.isRequired,
+    pass: PropTypes.string.isRequired,
+
+    
+}
---
+++ src/components/password-reset/PasswordReset.comp.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+import { PropTypes } from "prop-types";
+
+
+export const ResetPassword = ({handleOnchange,handleOnResetSubmit,formSwitcher, email}) => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>   
+                        <h1 className="text-info text-center">Reset Password</h1>
+                        <hr />
+                        <Form autoComplete="off" onSubmit={handleOnResetSubmit}>
+                            <Form.Group>
+                                <Form.Label>
+                                    Email Addres
+                                </Form.Label>
+                                <Form.Control type="email"
+                                name="email" 
+                                value={email}
+                                onChange={handleOnchange}
+                                placeholder="Enter Email" 
+                                required
+                                />
+                            </Form.Group>
+
+                            
+
+                            <Button type="submit">Reset Password</Button>
+
+
+                        </Form>
+                        <hr />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <a href="hgy" onClick={() => formSwitcher("login")}>Loging Now</a>
+                    </Col>
+                </Row>
+                
+            </Container>
+        </div>
+    )
+}
+
+ResetPassword.propTypes = {
+    handleOnchange: PropTypes.func.isRequired,
+    handleOnResetSubmit: PropTypes.func.isRequired,
+    formSwitcher: PropTypes.func.isRequired,
+    email: PropTypes.string.isRequired,
+    
+    
+}
