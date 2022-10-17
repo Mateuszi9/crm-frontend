30ee7695cf1b57b49d1e3a22195e0369b19b7ba4
Mateusz Izdebski <mati2k51@gmail.com>
2022-10-17 09:51:29 +0200

    breadcrumb added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 
 import { Entry } from './pages/entry/Entry.page';
 
@@ -10,7 +11,7 @@ function App() {
       {/*<Entry />*/}
       
       <DefaultLayout>
-        //Dashboard
+        <Dashboard/>
       </DefaultLayout>
     </div>
   );
